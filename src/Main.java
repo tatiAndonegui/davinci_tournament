0dd53a5cd73f0233fc01396fe34fdf90f5a69074
tatiana <115169557+tatiAndonegui@users.noreply.github.com>
2022-10-20 12:00:45 -0300

    ordenamiento de main

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,16 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Da vinci Tournament");
+
         Date comienzoDeCursada = new Date(2022,7,18);
         Date finDeCursada = new Date(2022,11,5);
+
         Season torneoDaVinci = new Season(
                 "Torneo 2022, 2do cuatri",
                 comienzoDeCursada,
                 finDeCursada);
 
-
-
-
-        torneoDaVinci.setStartDate(comienzoDeCursada);
         
-
-        torneoDaVinci.setEndDate(finDeCursada);
-
 
     }
 }
