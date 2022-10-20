de31e410f2059a66a0d94e70f16870a1df108e65
tatiana <115169557+tatiAndonegui@users.noreply.github.com>
2022-10-20 11:55:42 -0300

    refactorizacion (no cambio nada a nivel funcional).update dat definitions

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,19 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Da vinci Tournament");
-        Date comienzoDeCursada = new Date();
-        Date finDeCursada = new Date();
+        Date comienzoDeCursada = new Date(2022,7,18);
+        Date finDeCursada = new Date(2022,11,5);
         Season torneoDaVinci = new Season(
                 "Torneo 2022, 2do cuatri",
                 comienzoDeCursada,
                 finDeCursada);
 
 
-        comienzoDeCursada.setYear(2022);
-        comienzoDeCursada.setMonth(7); //Enero 0, Agosto 7
-        comienzoDeCursada.setDate(18);
-        torneoDaVinci.setStartDate(comienzoDeCursada);
 
-        finDeCursada.setYear(2022);
-        finDeCursada.setMonth(11); //Enero 0, Dic 11
-        finDeCursada.setDate(5);
+
+        torneoDaVinci.setStartDate(comienzoDeCursada);
+        
+
         torneoDaVinci.setEndDate(finDeCursada);
 
 
