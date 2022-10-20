dc6e6a60675420bc18f1f40ea92036b5ad0bc281
tatiana <115169557+tatiAndonegui@users.noreply.github.com>
2022-10-20 12:01:32 -0300

    feat: add setter y gettes de season

--- src/Season.java
+++ src/Season.java
@@ -39,5 +39,20 @@ class Season {
         return ;
     }
 
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public ArrayList<Match> getMatchs() {
+        return matchs;
+    }
+
+    public void setMatchs(ArrayList<Match> matchs) {
+        this.matchs = matchs;
+    }
 }
 
